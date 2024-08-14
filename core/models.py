ffbd3f5fa5b05d64d9822331e1cb54d7b0ce4153
kimemia21 <bobbymbogo71@gmail.com>
2024-08-14 16:29:52 +0300

    added or rather started on the models

--- core/models.py
+++ core/models.py
@@ -20,17 +20,81 @@ class  Student(models.Model):
         
 
 class user(models.Model):
-    selections =[('buyer',"Buyer"),('seller',"Seller"),('producer',"Producer")]
+    # selections =[('buyer',"Buyer"),('seller',"Seller"),('producer',"Producer")]
    
-    
-    email =models.EmailField(max_length=50,null=True,default="test")
-    password =models.CharField(max_length=50,null=True,default="test")
+      
+    email =models.EmailField(max_length=255,null=True,default="test")
+    password =models.CharField(max_length=100,null=True,default="test")
     c2fc =models.CharField(max_length=8,null=True,default="test")
-    phone_number =models.CharField(max_length=20,null=True,default="test")
-    role =models.CharField(max_length=50,default="buyer")
+    # phone_number =models.CharField(max_length=20,null=True,default="test")
+    # role =models.CharField(max_length=50,default="seller")
      
     def __str__(self):
         return self.email
+ 
+class commodites(models.Model):
+    id =models.IntegerField(max_length=255, primary_key=True)
+    name  =models.CharField( max_length=255)
+    commodity_type ="Foreign key from commodites_type"
+    description=models.TextField()
+    icon_name =models.CharField(max_length=100)
+    image_url =models.CharField( max_length=255)
+    commodity_primary_packing_id =  id
+ 
+    
+class user_has_commodity(models.Model):
+    user_id =models.ForeignKey(user, verbose_name=_(""), on_delete=models.CASCADE)    
+    commodity_id =models.ForeignKey(commodites , on_delete=models.CASCADE)
+    user_has_commodity_id =models.IntegerField(primary_key=True,max_length=11)
+    
+    
+class jurisdictions(models.Model):
+    jurisdictions_name =models.CharField( max_length=100)
+    description=models.TextField()
+   
+    
+
+class banking_profile(models.Model):
+    user_id= models.ForeignKey(user, on_delete=models.CASCADE)
+    banking_profile_id =models.IntegerField(max_length=11,primary_key=True)
+    bank_branch_id=models.CharField( max_length=10)
+    swift =models.CharField( max_length=30)
+    IBAN =models.CharField( max_length=100)
+    
+class user_has_usertype(models.Model):
+    usertypeid ="foreignkey from usertypes"
+    user_id =models.ForeignKey(user , on_delete=models.CASCADE)
+    
+        
+    
+    
+class sale_packaging(models.Model):
+    packing_id =models.ForeignKey(jurisdictions, on_delete=models.CASCADE)
+    packing_name =models.CharField( max_length=100)
+    commodites_id=models.ForeignKey(commodites, verbose_name=_(""), on_delete=models.CASCADE) 
+    
+
+ 
+class countries(models.Model):
+    name =models.CharField( max_length=100)
+        
+    
+class country_is_within_jurisdiction(models.Model):
+    
+        
+           
+    
+    
+    
+    
+        
+    
+    
+# class user_has_usertype(models.Model):
+#     usertypeid =models.ForeignKey("model.user", on_delete=models.CASCADE) 
+        
+    
+    
 
    
 
--- core/views.py
+++ core/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
-from .models import Student
+from django.shortcuts import get_object_or_404
+from .models import Student,user
 from .Serializers import StudentSeriailizer
+from  django.contrib.auth.models import User
+
 
 class StudentApiView(APIView):
     def get_object(self,pk):
@@ -45,6 +48,26 @@ class StudentApiView(APIView):
             return Response({"message":"user deleted"})
         else:
             return Response(serializer.errors)     
+        
+        
+ 
+class Login(APIView):
+    def get(request):
+        data =request.data
+        try:
+            User =user.objects.get(email=data.get("email"))
+            
+            
+        
+            
+        except user.DoesNotExist:
+            return Response({"not found":data["email"]}, Http404)
+        
+    
+                 
+        
+        
+        
         
         
         
